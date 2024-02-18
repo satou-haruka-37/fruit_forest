97dc69d75cc714972b587e5e659f9dfdab4a9f36
satou haruka <haruka.satou.37@icloud.com>
2024-02-18 13:08:20 +0900

    🍓 renderデプロイのため修正4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,6 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  config.hosts << "fruit-forest.onrender.com"
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "app_production"
@@ -82,4 +81,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.hosts << "fruit-forest.onrender.com"
 end
